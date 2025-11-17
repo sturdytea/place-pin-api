612d88adc6fcd8da08b7c9c411aadc23f5a34ac0
sturdytea <aitmazh@gmail.com>
2025-11-18 01:46:16 +0500

    add fluent db

--- Package.swift
+++ Package.swift
@@ -11,6 +11,9 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "4.115.0"),
         // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
+        // Database dependencies
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.11.0"),
+        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.9.0")
     ],
     targets: [
         .executableTarget(
@@ -19,6 +22,9 @@ let package = Package(
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "NIOCore", package: "swift-nio"),
                 .product(name: "NIOPosix", package: "swift-nio"),
+                // Database dependencies
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver")
             ],
             swiftSettings: swiftSettings
         ),
